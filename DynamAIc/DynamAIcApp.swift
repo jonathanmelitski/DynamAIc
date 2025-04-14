9a56fdb451ea5fdb5af5bb97131e6cc5523db16d
Jonathan Melitski <jonathanmelitski@gmail.com>
2025-04-14 13:49:05 -0400

    Initial commit

---
+++ DynamAIc/DynamAIcApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamAIcApp.swift
+//  DynamAIc
+//
+//  Created by Jonathan Melitski on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamAIcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
